727cbab3d0767054524524ab00546a1e980357db
nicolas2lee <nicolas2lee@gmail.com>
2019-12-26 15:37:13 +0100

    [terraform] update terraform for ae

--- cos/analytics-engine/main.tf
+++ cos/analytics-engine/main.tf
@@ -4,7 +4,14 @@ data "ibm_resource_group" "group" {
 
 resource "ibm_resource_instance" "testacc_ds_resource_instance" {
   name              = "analytics-engine"
-  location          = "global"
+  location          = "eu-de"
   resource_group_id = "${data.ibm_resource_group.group.id}"
   service           = "ibmanalyticsengine"
+  plan              = "lite"
+
+  parameters = {
+    "num_compute_nodes" = 1
+    "hardware_config" = "default"
+    "software_package" = "ae-1.2-hadoop-spark"
+  }
 }
